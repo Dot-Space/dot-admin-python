5955c2bb03d19f33a2b7ca1f498b94b683611fb5
Andrew Balin <i@andrew-balin.ru>
2024-11-05 22:19:32 +0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dot-admin',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='A Django extension that adds custom filters functionality.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Dot-Space/dot-admin-python',
+    author='Dot.Space',
+    author_email='i@andrew-balin.ru',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=[
+        'django>=4.0',
+    ],
+)
