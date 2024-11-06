03cf44a72b85f9c142333399e2a99546b9540db0
Andrew Balin <i@andrew-balin.ru>
2024-11-06 12:01:58 +0500

    Обновил метакласс

--- dot_admin/filters/filterable/meta.py
+++ dot_admin/filters/filterable/meta.py
@@ -1,5 +1,12 @@
-class FilterableMeta(type):
+from django.db import models
+
+
+class FilterableMetaclass(type):
     def __new__(cls, name, bases, dct):
         if 'FilterField' in dct:
             dct['_filter_field'] = dct['FilterField']
         return super().__new__(cls, name, bases, dct)
+
+
+class FilterableMeta(FilterableMetaclass, models.base.ModelBase):
+    pass
