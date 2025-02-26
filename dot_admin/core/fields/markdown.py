3443bb3144b7647f7bb650b3336ee7265b3cdcc9
Балин Андрей <balin_av@devtrix.ru>
2025-02-26 17:27:44 +0500

    Обновление сериализатора MD

--- dot_admin/core/fields/markdown.py
+++ dot_admin/core/fields/markdown.py
@@ -5,7 +5,7 @@ import markdown
 class MarkdownField(CharField):
     # TODO: Создать MD editor для DRF
     def to_representation(self, value):
-        return markdown.markdown(value)
+        return value
 
     def to_internal_value(self, data):
         return str(data)
